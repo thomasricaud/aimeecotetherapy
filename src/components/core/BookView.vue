26a9c0fd0d7f12582dff847ac03bb96aab8c1959
Thomas Ricaud <thomas.ricaud@gadz.org>
2020-08-05 20:38:06 +0200

    update session and fees

--- src/components/core/BookView.vue
+++ src/components/core/BookView.vue
@@ -9,7 +9,7 @@
       <li>{{ $i18n.t("Book3") }}</li>
       <li>{{ $i18n.t("Book4") }}</li>
       <li>{{ $i18n.t("Book5") }}</li>
-      <li>{{ $i18n.t("Book6") }}</li>
+      <p>{{ $i18n.t("Book6") }}</p>
     </base-text>
     <form
       v-if="!savingSuccessful"
@@ -33,11 +33,7 @@
         @input="$v.email.$touch()"
         @blur="$v.email.$touch()"
       />
-      <v-col
-        cols="12"
-        sm="6"
-        md="4"
-      >
+      <v-col cols="12" sm="6" md="4">
         <v-menu
           v-model="menu2"
           :close-on-content-click="true"
@@ -56,10 +52,7 @@
               v-on="on"
             />
           </template>
-          <v-date-picker
-            v-model="date"
-            @input="menu2 = false"
-          />
+          <v-date-picker v-model="date" @input="menu2 = false" />
         </v-menu>
       </v-col>
       <v-textarea
@@ -70,106 +63,101 @@
         @blur="$v.textarea.$touch()"
       />
 
-      <v-btn
-        class="accent mr-4"
-        type="submit"
-        value="Send"
-      >
+      <v-btn class="accent mr-4" type="submit" value="Send">
         send
       </v-btn>
-      <v-btn
-        class="accent mr-4"
-        @click="clear"
-      >
+      <v-btn class="accent mr-4" @click="clear">
         clear
       </v-btn>
     </form>
-    <v-alert
-      v-if="savingSuccessful"
-      type="success"
-    >
+    <v-alert v-if="savingSuccessful" type="success">
       Your email was sent we will respond shortly
     </v-alert>
   </section>
 </template>
 <script>
-  import { validationMixin } from 'vuelidate'
-  import { required, maxLength, email, alphaNum } from 'vuelidate/lib/validators'
-  import emailjs from 'emailjs-com'
-  export default {
-    mixins: [validationMixin],
+import { validationMixin } from "vuelidate";
+import { required, maxLength, email, alphaNum } from "vuelidate/lib/validators";
+import emailjs from "emailjs-com";
+export default {
+  mixins: [validationMixin],
 
-    validations: {
-      name: { required, maxLength: maxLength(50) },
-      email: { required, email },
-      date: { },
-      textarea: { alphaNum },
+  validations: {
+    name: { required, maxLength: maxLength(50) },
+    email: { required, email },
+    date: {},
+    textarea: { alphaNum }
+  },
+  data: () => ({
+    name: "",
+    email: "",
+    select: null,
+    items: ["Item 1", "Item 2", "Item 3", "Item 4"],
+    textarea: "",
+    date: new Date().toISOString().substr(0, 10),
+    menu: false,
+    modal: false,
+    menu2: false,
+    savingSuccessful: false
+  }),
+  computed: {
+    dateErrors() {
+      const errors = [];
+      if (!this.$v.date.$dirty) return errors;
+      return errors;
     },
-    data: () => ({
-      name: '',
-      email: '',
-      select: null,
-      items: [
-        'Item 1',
-        'Item 2',
-        'Item 3',
-        'Item 4',
-      ],
-      textarea: '',
-      date: new Date().toISOString().substr(0, 10),
-      menu: false,
-      modal: false,
-      menu2: false,
-      savingSuccessful: false,
-    }),
-    computed: {
-      dateErrors () {
-        const errors = []
-        if (!this.$v.date.$dirty) return errors
-        return errors
-      },
-      textareaErrors () {
-        const errors = []
-        if (!this.$v.textarea.$dirty) return errors
-        return errors
-      },
-      nameErrors () {
-        const errors = []
-        if (!this.$v.name.$dirty) return errors
-        !this.$v.name.maxLength && errors.push('Name must be at most 50 characters long')
-        !this.$v.name.required && errors.push('Name is required.')
-        return errors
-      },
-      emailErrors () {
-        const errors = []
-        if (!this.$v.email.$dirty) return errors
-        !this.$v.email.email && errors.push('Must be valid e-mail')
-        !this.$v.email.required && errors.push('E-mail is required')
-        return errors
-      },
+    textareaErrors() {
+      const errors = [];
+      if (!this.$v.textarea.$dirty) return errors;
+      return errors;
     },
+    nameErrors() {
+      const errors = [];
+      if (!this.$v.name.$dirty) return errors;
+      !this.$v.name.maxLength &&
+        errors.push("Name must be at most 50 characters long");
+      !this.$v.name.required && errors.push("Name is required.");
+      return errors;
+    },
+    emailErrors() {
+      const errors = [];
+      if (!this.$v.email.$dirty) return errors;
+      !this.$v.email.email && errors.push("Must be valid e-mail");
+      !this.$v.email.required && errors.push("E-mail is required");
+      return errors;
+    }
+  },
 
-    methods: {
-      sendEmail (e) {
-        emailjs.sendForm('gmail', 'template_W4aisDYL', e.target, 'user_uCSQHcEpjWGwhD3pfhcYT')
-          .then((result) => {
-            console.log('SUCCESS!', result.status, result.text)
-            this.clear()
-            this.savingSuccessful = true
-          }, (error) => {
-            console.log('FAILED...', error)
-          })
-      },
-      submit () {
-        this.$v.$touch()
-      },
-      clear () {
-        this.$v.$reset()
-        this.name = ''
-        this.email = ''
-        this.textarea = ''
-        this.date = ''
-      },
+  methods: {
+    sendEmail(e) {
+      emailjs
+        .sendForm(
+          "gmail",
+          "template_W4aisDYL",
+          e.target,
+          "user_uCSQHcEpjWGwhD3pfhcYT"
+        )
+        .then(
+          result => {
+            console.log("SUCCESS!", result.status, result.text);
+            this.clear();
+            this.savingSuccessful = true;
+          },
+          error => {
+            console.log("FAILED...", error);
+          }
+        );
     },
+    submit() {
+      this.$v.$touch();
+    },
+    clear() {
+      this.$v.$reset();
+      this.name = "";
+      this.email = "";
+      this.textarea = "";
+      this.date = "";
+    }
   }
+};
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,60 +1,59 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    articles: require('@/data/articles.json'),
+    articles: require("@/data/articles.json"),
     drawer: false,
     items: [
       {
-        text: 'Home',
-        href: '/home',
+        text: "Home",
+        href: "/home"
       },
- //     {
- //       text: 'Blog',
- //       href: '/blog',
- //     },
+      //     {
+      //       text: 'Blog',
+      //       href: '/blog',
+      //     },
       {
-        text: 'Book a session',
-        href: '/book',
+        text: "Contact me",
+        href: "/book"
       },
       {
-        text: 'Getting there',
-        href: '/there',
-      },
-    ],
+        text: "Getting there",
+        href: "/there"
+      }
+    ]
   },
   getters: {
     categories: state => {
-      const categories = []
+      const categories = [];
 
       for (const article of state.articles) {
         if (
           !article.category ||
           categories.find(category => category.text === article.category)
-        ) continue
+        )
+          continue;
 
-        const text = article.category
+        const text = article.category;
 
         categories.push({
           text,
-          href: text,
-        })
+          href: text
+        });
       }
 
-      return categories.sort().slice(0, 4)
+      return categories.sort().slice(0, 4);
     },
     links: (state, getters) => {
-      return state.items
-    },
+      return state.items;
+    }
   },
   mutations: {
     setDrawer: (state, payload) => (state.drawer = payload),
-    toggleDrawer: state => (state.drawer = !state.drawer),
+    toggleDrawer: state => (state.drawer = !state.drawer)
   },
-  actions: {
-
-  },
-})
+  actions: {}
+});
