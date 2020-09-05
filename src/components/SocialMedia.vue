057abf0b50a45ad0762bfb6ad0d40c11534b19fc
Thomas Ricaud <thomas.ricaud@gadz.org>
2020-09-05 10:48:36 +0200

    update phone number

--- src/components/SocialMedia.vue
+++ src/components/SocialMedia.vue
@@ -8,53 +8,50 @@
       color="info"
       :small="!large"
       target="_blank"
-      :href= item.src
+      :href="item.src"
     >
-      <v-icon
-        dark
-        v-text="item.icon"
-      />
+      <v-icon dark v-text="item.icon" />
     </v-btn>
   </div>
 </template>
 
 <script>
-  export default {
-    props: {
-      large: {
-        type: Boolean,
-        default: false,
-      },
-    },
+export default {
+  props: {
+    large: {
+      type: Boolean,
+      default: false
+    }
+  },
 
-    data: () => ({
-      items: [
-        {
-          name: 'Messenger',
-          icon: 'mdi-facebook-messenger',
-          src: 'https://www.messenger.com/t/AimeeCoteTherapy',
-        },
-        {
-          name: 'Instagram',
-          icon: 'mdi-instagram',
-          src: 'https://www.instagram.com/aimee_cote_therapy',
-        },
-        {
-          name: 'Facebook',
-          icon: 'mdi-facebook',
-          src: 'https://www.facebook.com/AimeeCoteTherapy',
-        },
-        {
-          name: 'Email',
-          icon: 'mdi-email',
-          src: 'mailto:aimee.cote.therapy@gmail.com',
-        },
-        {
-          name: 'Phone',
-          icon: 'mdi-phone',
-          src: 'tel:+33621457898',
-        },
-      ],
-    }),
-  }
+  data: () => ({
+    items: [
+      {
+        name: "Messenger",
+        icon: "mdi-facebook-messenger",
+        src: "https://www.messenger.com/t/AimeeCoteTherapy"
+      },
+      {
+        name: "Instagram",
+        icon: "mdi-instagram",
+        src: "https://www.instagram.com/aimee_cote_therapy"
+      },
+      {
+        name: "Facebook",
+        icon: "mdi-facebook",
+        src: "https://www.facebook.com/AimeeCoteTherapy"
+      },
+      {
+        name: "Email",
+        icon: "mdi-email",
+        src: "mailto:aimee.cote.therapy@gmail.com"
+      },
+      {
+        name: "Phone",
+        icon: "mdi-phone",
+        src: "tel:+33651417925"
+      }
+    ]
+  })
+};
 </script>
