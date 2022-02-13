1b069f047dc7b17dabf4c5827903727507d2791a
Thomas Ricaud <thomas.ricaud@gadz.org>
2022-02-13 18:22:23 +0100

    My approach transalation file

--- src/components/MyApproach.vue
+++ src/components/MyApproach.vue
@@ -16,8 +16,7 @@
         </base-heading>
         <base-text>
           <br><br>
-          <p>I completed my MS in Marriage and Family Therapy at the University of New Hampshire in 2009. Marriage and family therapy differs from other types of psychotherapy in that it focuses on interactions between people who, together, form a system. In fact, marriage and family therapy is often referred to as "thérapie systémique" here in France. However, there are no masters programs in this specialty in France. Marriage and family therapists (MFTs) consider the client in the context of his or her family or other close relationships.</p>
-          <p>In our families, we often take on roles that may limit our happiness and feeling of being loved and cared for. While systemic therapy is ideal for working with families and couples, it is also effective for individuals. After all, many problems we face as individuals have to do with our relationships.</p>
+          <p v-html="$i18n.t('myapproach')" />
         </base-text>
       </v-container>
       <v-container>
@@ -28,7 +27,7 @@
         />
         <base-text>
           <br><br><br><br>
-          <p>The models I favor tend to be very "client-centered." That means that I put you and your goals first, rather than imposing my ideas on you. In other words, I will make suggestions, but they must feel right to you, and ultimately, the choice is yours. I am here to guide you and your family, not to "fix" you, because you are not broken. You and your family members possess a wealth of resources and strengths that I will help you uncover.</p>
+          <p v-html="$i18n.t('myapproach2')" />
         </base-text>
       </v-container>
     </v-row>
