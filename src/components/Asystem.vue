42b08b17a85bb9047388f25febc1429f8ef174fa
Thomas Ricaud <thomas.ricaud@gadz.org>
2025-01-11 11:18:35 +0100

    Changement de numéro de téléphone

--- src/components/Asystem.vue
+++ src/components/Asystem.vue
@@ -9,32 +9,14 @@
       class="float-md-right mx-auto my-12 ma-md-12"
     />
     <base-heading>
-      <h1>How is a family like a system?</h1>
+      <h1>{{ $i18n.t("ASystem") }}</h1>
     </base-heading>
     <base-text>
       <p>
-        Marriage and family therapy is known as "systemic therapy" in France.
-        This is because marriage and family therapists, or MFT's, see families
-        as systems of people and relationships. Like other types of systems,
-        even mechanical ones, families try to maintain balance or equilibrium.
-        When changes arise, the system quickly tries to re-balance itself. A
-        common example of this is the thermostat in your home. The system is at
-        equilibrium when it's at the set temperature on the thermostat. When the
-        temperature drops , the heating system kicks on. When the thermostat
-        senses the home has reached the right temperature, the heat shuts off.
-        Equilibrium is back.
+        {{ $i18n.t("ASystem1") }}
       </p>
       <p>
-        We can apply this idea to a family confronting a problem. If one family
-        member is struggling with depression, for example, other family members
-        may alter their emotional state or behavior to get the family back into
-        balance. Some ways are more conducive to mental health than others. For
-        instance, in one family, people might reach out to the depressed
-        individual to offer support. But in another family, members might
-        respond by also becoming depressed as a way to share the burden. As you
-        can imagine, this probably won't be effective at restoring harmony. When
-        the family's response to a problem drives the system further out of
-        balance, this is when therapy can be helpful.
+        {{ $i18n.t("ASystem2") }}
       </p>
     </base-text>
   </section>
--- src/components/SocialMedia.vue
+++ src/components/SocialMedia.vue
@@ -49,7 +49,7 @@ export default {
       {
         name: "Phone",
         icon: "mdi-phone",
-        src: "tel:+33651417925"
+        src: "tel:+33622701183"
       }
     ]
   })
