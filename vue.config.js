8f994101afcb9b4ae833e27a20b6b5830da7348f
Thomas Ricaud <thomas.ricaud@gadz.org>
2025-09-06 21:54:52 +0200

    prerending install pugin

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   devServer: {
-    disableHostCheck: true,
+    // `disableHostCheck` was removed in webpack-dev-server v4.
+    // Allow all hosts during local development.
+    allowedHosts: 'all',
   },
 
   transpileDependencies: ['vuetify'],
@@ -11,7 +13,10 @@ module.exports = {
       fallbackLocale: 'en',
       localeDir: 'locales',
       enableInSFC: true
-    }
+    },
+    // Placeholder to prevent vue-cli-plugin-prerender-spa from crashing when no config provided.
+    // We will configure routes and options later.
+    prerenderSpa: {}
   },
 
   pages: {
