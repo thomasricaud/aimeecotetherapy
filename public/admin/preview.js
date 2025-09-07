c210c5740b8b99491f72cb07772c76d865fa31a7
Thomas RICAUD <thomas.ricaud@gadz.org>
2025-09-07 10:26:03 +0200

    feat: add blog description to CMS

--- public/admin/preview.js
+++ public/admin/preview.js
@@ -1,16 +1,23 @@
-CMS.registerPreviewStyle("/admin/preview.css");
+CMS.registerPreviewStyle('/admin/preview.css');
 
 const BlogPreview = createClass({
-  render: function() {
-    const entry = this.props.entry;
-    const image = entry.getIn(['data', 'image']);
-    const imageUrl = image && image !== 'hide' ? this.props.getAsset(image) : null;
+  shouldComponentUpdate (nextProps) {
+    const current = this.props.entry.get('data').delete('body');
+    const next = nextProps.entry.get('data').delete('body');
+    return !current.equals(next);
+  },
+  render () {
+    const { entry, widgetFor, getAsset } = this.props;
+    const data = entry.get('data');
+    const image = data.get('image');
+    const imageUrl = image && image !== 'hide' ? getAsset(image) : null;
     return h('div', {},
-      h('div', { className: 'category' }, entry.getIn(['data', 'category'])),
-      h('h1', { className: 'title' }, entry.getIn(['data', 'title'])),
-      h('p', { className: 'author' }, entry.getIn(['data', 'author'])),
-      h('div', { className: 'body' }, this.props.widgetFor('body')),
-      imageUrl ? h('img', { className: 'image', src: imageUrl.toString(), alt: entry.getIn(['data', 'title']) }) : null
+      h('div', { className: 'category' }, data.get('category')),
+      h('h1', { className: 'title' }, data.get('title')),
+      h('p', { className: 'author' }, data.get('author')),
+      h('p', { className: 'description' }, data.get('description')),
+      h('div', { className: 'body' }, widgetFor('body')),
+      imageUrl ? h('img', { className: 'image', src: imageUrl.toString(), alt: data.get('title') }) : null
     );
   }
 });
