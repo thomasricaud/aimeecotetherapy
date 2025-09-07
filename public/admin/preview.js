2f879700110cf0bc9358e32e66c962c3597988e8
Thomas Ricaud <thomas.ricaud@gadz.org>
2025-09-07 10:49:53 +0200

    preiview

--- public/admin/preview.js
+++ public/admin/preview.js
@@ -1,24 +1,37 @@
 CMS.registerPreviewStyle('/admin/preview.css');
 
-const BlogPreview = createClass({
-  shouldComponentUpdate (nextProps) {
-    const current = this.props.entry.get('data').delete('body');
-    const next = nextProps.entry.get('data').delete('body');
-    return !current.equals(next);
-  },
-  render () {
-    const { entry, widgetFor, getAsset } = this.props;
-    const data = entry.get('data');
-    const image = data.get('image');
-    const imageUrl = image && image !== 'hide' ? getAsset(image) : null;
-    return h('div', {},
-      h('div', { className: 'category' }, data.get('category')),
-      h('h1', { className: 'title' }, data.get('title')),
-      h('p', { className: 'author' }, data.get('author')),
-      h('div', { className: 'body' }, widgetFor('body')),
-      imageUrl ? h('img', { className: 'image', src: imageUrl.toString(), alt: data.get('title') }) : null
-    );
+// Passive, memoized preview component (no side effects)
+const BlogPreviewBase = (props) => {
+  const { entry, widgetFor, getAsset } = props;
+  const data = entry.get('data');
+  const image = data.get('image');
+
+  let imageSrc = null;
+  if (image && image !== 'hide') {
+    const asset = getAsset(image);
+    if (asset) {
+      if (typeof asset === 'string') imageSrc = asset;
+      else if (asset.url) imageSrc = asset.url;
+      else if (typeof asset.toString === 'function') imageSrc = asset.toString();
+    }
   }
-});
+
+  return h('div', {},
+    h('div', { className: 'category' }, data.get('category')),
+    h('h1', { className: 'title' }, data.get('title')),
+    h('p', { className: 'author' }, data.get('author')),
+    h('div', { className: 'body' }, widgetFor('body')),
+    imageSrc ? h('img', { className: 'image', src: imageSrc, alt: data.get('title') }) : null
+  );
+};
+
+// Custom props equality: ignore body/description to avoid cursor jumps
+const areEqual = (prevProps, nextProps) => {
+  const prevData = prevProps.entry.get('data').delete('body').delete('description');
+  const nextData = nextProps.entry.get('data').delete('body').delete('description');
+  return prevData.equals(nextData);
+};
+
+const BlogPreview = React.memo(BlogPreviewBase, areEqual);
 
 CMS.registerPreviewTemplate('blog', BlogPreview);
