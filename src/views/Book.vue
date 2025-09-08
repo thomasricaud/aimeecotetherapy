c2ed2ba9a54f5790fe6aa1123bb3c841a2feaa17
Thomas RICAUD <thomas.ricaud@gadz.org>
2025-09-08 10:17:47 +0200

    Include locale-aware structured data

--- src/views/Book.vue
+++ src/views/Book.vue
@@ -5,6 +5,8 @@ class="mt-10"
 </template>
 
 <script>
+  import buildGraphFor from '@/buildGraphFor'
+
   export default {
     name: 'Book',
     components: {
@@ -13,6 +15,8 @@ class="mt-10"
     metaInfo () {
       const path = this.$route?.path || '/'
       const canonical = `https://aimeecotetherapy.com${path.endsWith('/') ? path : path + '/'}`
+      const routeName = this.$route?.name
+      const lang = this.$i18n?.locale || this.$route?.params.lang || 'en'
       return {
         title: this.$t('meta.bookTitle'),
         meta: [
@@ -23,6 +27,12 @@ class="mt-10"
         ],
         link: [
           { vmid: 'canonical', rel: 'canonical', href: canonical }
+        ],
+        script: [
+          {
+            type: 'application/ld+json',
+            json: buildGraphFor(routeName, lang, this.$t.bind(this))
+          }
         ]
       }
     }
--- src/views/There.vue
+++ src/views/There.vue
@@ -3,6 +3,8 @@
 </template>
 
 <script>
+  import buildGraphFor from '@/buildGraphFor'
+
   export default {
     name: 'There',
     components: {
@@ -11,6 +13,8 @@
     metaInfo () {
       const path = this.$route?.path || '/'
       const canonical = `https://aimeecotetherapy.com${path.endsWith('/') ? path : path + '/'}`
+      const routeName = this.$route?.name
+      const lang = this.$i18n?.locale || this.$route?.params.lang || 'en'
       return {
         title: this.$t('meta.thereTitle'),
         meta: [
@@ -21,6 +25,12 @@
         ],
         link: [
           { vmid: 'canonical', rel: 'canonical', href: canonical }
+        ],
+        script: [
+          {
+            type: 'application/ld+json',
+            json: buildGraphFor(routeName, lang, this.$t.bind(this))
+          }
         ]
       }
     }
